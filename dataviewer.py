c7a2b21725152d4919b1ee18316408c711da5abd
Preston Roesslet <proesslet@gmail.com>
2023-02-13 16:48:12 -0600

    Started new layout

--- dataviewer.py
+++ dataviewer.py
@@ -15,7 +15,7 @@ ws.connect("wss://ws.weatherflow.com/swd/data?token=" + token)
 result = ws.recv()
 
 # Default Settings
-temp_unit = "Fahrenheit"
+temp_unit = "F"
 wind_unit = "mph"
 pressure_unit = "inHg"
 rain_unit = "in"
@@ -25,56 +25,77 @@ window = tk.Tk()
 window.title("Weatherflow Data Viewer")
 window.minsize(500, 500)
 
+# Configure the grid
+window.columnconfigure(0, minsize=250, weight=1)
+window.columnconfigure(1, minsize=250, weight=1)
+
 # Create a title for the dataviewer
 label = tk.Label(master=window, text="Preston's WX", font="Arial 24")
-label.pack()
+label.grid(row=0, column=0, columnspan=2)
 # Last update time
 last_update = tk.Label(master=window, text="Last Updated: 00:00:00", font="Arial 12")
-last_update.pack()
+last_update.grid(row=1, column=0, columnspan=2)
 
 # Create a frame to hold the data
 frame = tk.Frame(master=window, width=500, height=500)
-frame.pack()
+frame.grid(row=2, column=0, columnspan=2)
 
 # Temperature data
 temp_frame = tk.Frame(master=frame, width=150, height=150)
-temp_frame.pack()
-temp_label = tk.Label(master=temp_frame, text="Temperature:")
-temp_label.pack()
+temp_frame.grid(row=2, column=0, padx=10, pady=10)
+temp_label = tk.Label(master=temp_frame, text="Temperature", font="Arial 16 underline")
+temp_label.grid(row=0, column=0)
 temp_data = tk.Label(master=temp_frame, text="0", font="Arial 20")
-temp_data.pack()
+temp_data.grid(row=1, column=0)
 
 # Wind Speed data
 wind_frame = tk.Frame(master=frame, width=100, height=100)
-wind_frame.pack()
-wind_label = tk.Label(master=wind_frame, text="Wind Speed:")
-wind_label.pack()
+wind_frame.grid(row=2, column=1, padx=10, pady=10)
+wind_label = tk.Label(master=wind_frame, text="Wind Speed", font="Arial 16 underline")
+wind_label.grid(row=0, column=0)
 wind_data = tk.Label(master=wind_frame, text="0", font="Arial 20")
-wind_data.pack()
+wind_data.grid(row=1, column=0)
 
 # Pressure data
 pressure_frame = tk.Frame(master=frame, width=100, height=100)
-pressure_frame.pack()
-pressure_label = tk.Label(master=pressure_frame, text="Pressure:")
-pressure_label.pack()
+pressure_frame.grid(row=2, column=3, padx=10, pady=10)
+pressure_label = tk.Label(master=pressure_frame, text="Pressure", font="Arial 16 underline")
+pressure_label.grid(row=0, column=0)
 pressure_data = tk.Label(master=pressure_frame, text="0", font="Arial 20")
-pressure_data.pack()
+pressure_data.grid(row=1, column=0)
 
 # Humidity data
 humidity_frame = tk.Frame(master=frame, width=100, height=100)
-humidity_frame.pack()
-humidity_label = tk.Label(master=humidity_frame, text="Humidity:")
-humidity_label.pack()
+humidity_frame.grid(row=3, column=0, padx=10, pady=10)
+humidity_label = tk.Label(master=humidity_frame, text="Humidity", font="Arial 16 underline")
+humidity_label.grid(row=0, column=0)
 humidity_data = tk.Label(master=humidity_frame, text="0", font="Arial 20")
-humidity_data.pack()
+humidity_data.grid(row=1, column=0)
 
 # Rain data
 rain_frame = tk.Frame(master=frame, width=100, height=100)
-rain_frame.pack()
-rain_label = tk.Label(master=rain_frame, text="Rain:")
-rain_label.pack()
+rain_frame.grid(row=3, column=1, padx=10, pady=10)
+rain_label = tk.Label(master=rain_frame, text="Rain", font="Arial 16 underline")
+rain_label.grid(row=0, column=0)
 rain_data = tk.Label(master=rain_frame, text="0", font="Arial 20")
-rain_data.pack()
+rain_data.grid(row=1, column=0)
+
+#Solar Radiation data
+solar_frame = tk.Frame(master=frame, width=100, height=100)
+solar_frame.grid(row=3, column=3, padx=10, pady=10)
+solar_label = tk.Label(master=solar_frame, text="Solar Radiation", font="Arial 16 underline")
+solar_label.grid(row=0, column=0)
+solar_data = tk.Label(master=solar_frame, text="0", font="Arial 20")
+solar_data.grid(row=1, column=0)
+
+# if window closed, close the websocket
+def on_closing():
+    ws.send('{"type":"listen_stop","device_id":' + tempest_ID + ',"id":"2098388936"}')
+    ws.close()
+    print("Websocket closed")
+    window.destroy()
+    return
+
 
 
 def on_error(ws, message):
@@ -95,10 +116,11 @@ def on_message(ws, message):
         pressure = result['obs'][0][6]
         humidity = result['obs'][0][8]
         rain_accumulation = result['obs'][0][18]
-        
+        solar_radiation = result['obs'][0][11]
+
 
         # Data Conversions
-        if temp_unit == "Fahrenheit":
+        if temp_unit == "F":
             temp = round((temp * 9/5) + 32, 2)
         else:
             temp = round(temp, 2)
@@ -157,6 +179,7 @@ def on_message(ws, message):
         pressure_data.config(text=str(pressure))
         humidity_data.config(text=str(humidity) + "%")
         rain_data.config(text=str(rain_accumulation) + ' inches')
+        solar_data.config(text=str(solar_radiation) + " W/m^2")
 
     return
 
@@ -171,6 +194,7 @@ def connection():
 t = Thread(target=connection)
 t.start()
 
+window.protocol("WM_DELETE_WINDOW", on_closing)
 window.mainloop()
 
 
